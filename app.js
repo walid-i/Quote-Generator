7a0023f10c9dfaca68a392d8fc5d3ea9e8b1f499
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2022-09-03 23:47:24 +0100

    added some more functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //test test test
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if( document.body.classList.contains("dark-theme")){
+        icon.src = "Images/sun.png";
+    }else{
+        icon.src = "Images/moon.png";
+    }
+}
