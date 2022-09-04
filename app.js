f13f956b9ab60c91bbf675cf6b728c535d4cb78c
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2022-09-04 21:49:40 +0100

    removed dark mode

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //test test test
 
-var icon = document.getElementById("icon");
+// var icon = document.getElementById("icon");
 
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if( document.body.classList.contains("dark-theme")){
-        icon.src = "Images/sun.png";
-    }else{
-        icon.src = "Images/moon.png";
-    }
-}
+// icon.onclick = function(){
+//     document.body.classList.toggle("dark-theme");
+//     if( document.body.classList.contains("dark-theme")){
+//         icon.src = "Images/sun.png";
+//         icon.src = "../Images/sun.png";
+//     }else{
+//         icon.src = "Images/moon.png";
+//         icon.src = "../Images/moon.png";
+//     }
+// }
