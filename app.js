8090858a3e70a25f64e4f0705dc2837e0bc6c797
Walid Islam <walidislam@Walids-MBP.lan>
2022-09-13 20:32:56 -0400

    each tab now gets a quote from its own database, home page chooses a random category

--- app.js
+++ app.js
@@ -1,14 +1,87 @@
 //test test test
 
-function generate(){
-    $.get('test.csv', function(CSVdata){
-        data = $.csv.toArray(CSVdata);
-        console.log(data)
-        randomNum = Math.floor(Math.random() * data.length)
-        console.log("i pick...")
-        console.log(data[randomNum])
-        document.getElementById("quoteAreaHome").innerHTML = data[randomNum]
-    })
+function generate(x){
+    console.log("reached here")
+    if(x == "random"){
+        var options = ["inspiration", "love", "mind", "sports"]
+        var random = Math.floor(Math.random() * options.length);
+        x = options[random];
+        console.log("random is " + x);
+        var flag = true
+    }
+    if(x == "inspiration"){
+        console.log("reached here inspiration")
+        var poop = {"separator":";"};
+        $.get('/quotes/inspiration.csv', function(CSVdata){
+            data = $.csv.toArray(CSVdata, poop);
+            console.log(data)
+            randomNum = Math.floor(Math.random() * data.length)
+            console.log("i pick...")
+            console.log(data[randomNum])
+            if(flag){
+                document.getElementById("quoteAreaHome").innerHTML = data[randomNum]
+            }
+            else{
+                document.getElementById("quoteAreaHomeInspiration").innerHTML = data[randomNum]
+            }
+            
+        })
+    }
+    else if(x == "love"){
+        console.log("reached here love")
+        var poop = {"separator":";"};
+        $.get('/quotes/love.csv', function(CSVdata){
+            data = $.csv.toArray(CSVdata, poop);
+            console.log(data)
+            randomNum = Math.floor(Math.random() * data.length)
+            console.log("i pick...")
+            console.log(data[randomNum])
+            if(flag){
+                document.getElementById("quoteAreaHome").innerHTML = data[randomNum]
+            }
+            else{
+                document.getElementById("quoteAreaHomeLove").innerHTML = data[randomNum]
+            }
+        })
+    }
+    else if(x == "mind"){
+        console.log("reached here mind")
+        var poop = {"separator":";"};
+        $.get('/quotes/mind.csv', function(CSVdata){
+            data = $.csv.toArray(CSVdata, poop);
+            console.log(data)
+            randomNum = Math.floor(Math.random() * data.length)
+            console.log("i pick...")
+            console.log(data[randomNum])
+            if(flag){
+                document.getElementById("quoteAreaHome").innerHTML = data[randomNum]
+            }
+            else{
+                document.getElementById("quoteAreaHomeMind").innerHTML = data[randomNum]
+            }
+        })
+    }
+    else if(x == "sports"){
+        console.log("reached here sports")
+        var poop = {"separator":";"};
+        $.get('/quotes/sports.csv', function(CSVdata){
+            data = $.csv.toArray(CSVdata, poop);
+            console.log(data)
+            randomNum = Math.floor(Math.random() * data.length)
+            console.log("i pick...")
+            console.log(data[randomNum])
+            if(flag){
+                document.getElementById("quoteAreaHome").innerHTML = data[randomNum]
+            }
+            else{
+                document.getElementById("quoteAreaHomeSports").innerHTML = data[randomNum]
+            }
+        })
+    }
+}
+
+function testFunction(){
+    console.log("reached here 2")
 }
 
 // var icon = document.getElementById("icon");
